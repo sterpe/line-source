84a80da1b7095244b018e16d4a189caaaa7c324c
sterpe <srterpe@gmail.com>
2016-03-06 17:38:45 -0800

    Depend on `string-source` package.

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,13 @@
 'use strict';
 
+var StringSource = require('string-source');
+
 var e = "Illegal '\\n' in line source.";
 
 module.exports = function LineSource(line) {
-  var position = 0;
-
   if (line.split('\n').length > 1) {
     throw new Error(e);
   }
 
-  return Object.defineProperty(Object.defineProperty({
-    peekChar: function peekChar() {
-      return position + 1 < line.length ? line.charAt(position + 1) : null;
-    },
-    nextChar: function nextChar() {
-      ++position;
-      return this.currentChar;
-    }
-  }, 'currentChar', {
-    get: function get() {
-      return position < line.length ? line.charAt(position) : null;
-    },
-    enumerable: true,
-    configurable: false
-  }), 'position', {
-    get: function get() {
-      return position < line.length ? position + 1 : null;
-    },
-    enumerable: true,
-    configurable: false
-  });
+  return new StringSource(line);
 };
--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 'use strict'
 
+const StringSource = require('string-source')
+
 const e = "Illegal '\\n' in line source."
 
 module.exports = function LineSource (line) {
-  let position = 0
-
   if (line.split('\n').length > 1) {
     throw new Error(e)
   }
 
-  return Object.defineProperty(Object.defineProperty({
-    peekChar: function () {
-      return position + 1 < line.length
-        ? line.charAt(position + 1)
-        : null
-    },
-    nextChar: function () {
-      ++position
-      return this.currentChar
-    }
-  }, 'currentChar', {
-    get: function () {
-      return (position < line.length)
-        ? line.charAt(position) : null
-    },
-    enumerable: true,
-    configurable: false
-  }), 'position', {
-    get: function () {
-      return (position < line.length)
-        ? position + 1 : null
-    },
-    enumerable: true,
-    configurable: false
-  })
+  return new StringSource(line)
 }
